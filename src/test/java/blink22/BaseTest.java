dbeda1bcb82a4c4f7b51e4f07965d9424c4eb2de
AbdullahMoghazy <abdullahmoghazy@icloud.com>
2025-07-02 01:54:37 +0300

    “Setting up the test suite”

---
+++ src/test/java/blink22/BaseTest.java
@@ -0,0 +1,27 @@
+package blink22;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.blink22.com/blog");
+        WebElement BlogTab = driver.findElement(By.linkText("Blog"));
+        BlogTab.click();
+        System.out.println("Page Title: " + driver.getTitle());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
